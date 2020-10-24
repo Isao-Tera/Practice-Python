b9d7edff51125e1716e479df93c94b04e83345b3
Isao-Tera <69697960+Isao-Tera@users.noreply.github.com>
2020-10-24 16:28:53 +0900

    Add files via upload
    Add how-to-use .iterrows, and how to make a new column in the pandas dataframe

--- pandas_intro.py
+++ pandas_intro.py
@@ -1,3 +1,31 @@
 import pandas as pd
 
-pd.read_csv("data.csv")
+test = pd.read_csv("data.csv")[:5]
+
+"""
+Since .iterrows() returns each DataFrame row as a tuple of (index, pandas Series) pairs, 
+you can either split this tuple and use the index and row-values separately
+"""
+
+for i, row in test.iterrows():
+    print(i)
+    print(row)
+    print(type(row))
+
+
+for row_tuple in test.iterrows():
+    print(row_tuple)
+    print(type(row_tuple))
+
+"""
+Create a new column
+"""
+total_visit = []
+for i, row in test.iterrows():
+    first_num = row["VISIT_NUM"]
+    second_num = row["VISIT_PAGE_NUM"]
+    visit_number = first_num + second_num
+    total_visit.append(visit_number)
+
+test["total_visit"] = total_visit
+print(test)
