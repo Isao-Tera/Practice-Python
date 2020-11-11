d54d8e941abcd19f816ab3e927590a4d0ee6d0ca
Isao-Tera <69697960+Isao-Tera@users.noreply.github.com>
2020-11-11 14:57:12 +0900

    Add files via upload

--- decorator.py
+++ decorator.py
@@ -23,3 +23,25 @@ def multiply(a, b):
     print(a * b)
 
 multiply(5, 10)
+
+# Make a decorator
+def print_return_type(func):
+  # Define wrapper(), the decorated function
+  def wrapper(*args, **kwargs):
+    # Call the function being decorated
+    result = func(*args, **kwargs)
+    print('{}() returned type {}'.format(
+      func.__name__, type(result)
+    ))
+    return result
+  # Return the decorated function
+  return wrapper
+  
+@print_return_type
+def foo(value):
+  return value
+  
+print(foo(42))
+print(foo([1, 2, 3]))
+print(foo({'a': 42}))
+
